64527d5020c8d1013ceec58b78ce6a076bdac4f7
Ogijun2018 <o2030028@edu.cc.uec.ac.jp>
2020-09-16 15:03:36 +0900

    migrationファイルいらない部分消去

--- db/migrate/20200915155108_remove_image_from_pictures.rb
+++
@@ -1,5 +0,0 @@
-class RemoveImageFromPictures < ActiveRecord::Migration[6.0]
-  def change
-    remove_column :pictures, :image, :string
-  end
-end
--- db/migrate/20200915173455_remove_episode_from_pictures.rb
+++
@@ -1,6 +0,0 @@
-class RemoveEpisodeFromPictures < ActiveRecord::Migration[6.0]
-  def change
-    remove_column :pictures, :episode, :integer
-    remove_column :pictures, :serif, :string
-  end
-end
