776e423f1c9e863f0adcb773fa4051881a846eb1
Ogijun2018 <o2030028@edu.cc.uec.ac.jp>
2021-04-07 01:16:17 +0900

    新規登録時にrootに戻らないように変更

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -22,6 +22,7 @@ class PicturesController < ApplicationController
   def create
     @picture = Picture.new(picture_params) # put into the data from form
     @picture.save
+    redirect_to new_picture_path
     # redirect_to root_path
   end
 
