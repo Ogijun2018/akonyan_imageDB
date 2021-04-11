bd4b649eb3a0a3b41d87d4eaa86061be968cdf75
Ogijun2018 <o2030028@edu.cc.uec.ac.jp>
2021-04-11 23:55:00 +0900

    ログイン機能を追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
   root "pictures#index"
   resources :pictures, only: [:new, :create, :destroy] do
     collection do
